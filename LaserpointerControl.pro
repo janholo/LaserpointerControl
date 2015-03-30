64cefb48900cd719a6c9fcee0dfbe99e6a32fbd3
raytracer1337 <jan.reinhardt1@gmx.de>
2015-03-30 21:21:54 +0200

    Added OpenGL Files

--- LaserpointerControl.pro
+++ LaserpointerControl.pro
@@ -13,9 +13,11 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    openglsimulation.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    openglsimulation.h
 
 FORMS    += mainwindow.ui
 
---
+++ openglsimulation.h
@@ -0,0 +1,21 @@
+#ifndef OPENGLSIMULATION_H
+#define OPENGLSIMULATION_H
+
+#include <QOpenGLWidget>
+
+class OpenGLSimulation : public QOpenGLWidget
+{
+    Q_OBJECT
+public:
+    explicit OpenGLSimulation(QObject *parent = 0);
+    ~OpenGLSimulation();
+
+    void Initialize
+
+signals:
+
+public slots:
+
+};
+
+#endif // OPENGLSIMULATION_H
