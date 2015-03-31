dc69615df2541b2340166d442a2c3bc10a5d2099
raytracer1337 <jan.reinhardt1@gmx.de>
2015-03-31 21:06:24 +0200

    OpenGL initalisation

--- openglsimulation.cpp
+++ openglsimulation.cpp
@@ -1,6 +1,7 @@
 #include "openglsimulation.h"
 
 OpenGLSimulation::OpenGLSimulation(QWidget *parent)
+    :QOpenGLWidget(parent)
 {
 
 }
@@ -12,12 +13,72 @@ OpenGLSimulation::~OpenGLSimulation()
 
 void OpenGLSimulation::initializeGL()
 {
-    glClearColor(0.4,0.4,0.4,1);
+    initializeOpenGLFunctions();
+
+    glClearColor(0.7, 0.7, 0.7, 1);
+
+    initShaders();
+    initTextures();
+
+    // Enable depth buffer
+    glEnable(GL_DEPTH_TEST);
+
+    // Enable back face culling
+    glEnable(GL_CULL_FACE);
+
+
+    // Use QBasicTimer because its faster than QTimer
+    timer.start(12, this);
+}
+
+void OpenGLSimulation::initShaders()
+{
+    // Compile vertex shader
+    if (!program.addShaderFromSourceFile(QOpenGLShader::Vertex, ":/shader/shader/vshader.glsl"))
+        close();
+
+    // Compile fragment shader
+    if (!program.addShaderFromSourceFile(QOpenGLShader::Fragment, ":/shader/shader/fshader.glsl"))
+        close();
+
+    // Link shader pipeline
+    if (!program.link())
+        close();
+
+    // Bind shader pipeline for use
+    if (!program.bind())
+        close();
+}
+
+void OpenGLSimulation::initTextures()
+{
+    // Load cube.png image
+    texture = new QOpenGLTexture(QImage(":images/images/servo.png"));
+
+    // Set nearest filtering mode for texture minification
+    texture->setMinificationFilter(QOpenGLTexture::Nearest);
+
+    // Set bilinear filtering mode for texture magnification
+    texture->setMagnificationFilter(QOpenGLTexture::Linear);
+
+    // Wrap texture coordinates by repeating
+    // f.ex. texture coordinate (1.1, 1.2) is same as (0.1, 0.2)
+    texture->setWrapMode(QOpenGLTexture::Repeat);
 }
 
 void OpenGLSimulation::resizeGL(int w, int h)
 {
+    // Calculate aspect ratio
+    qreal aspect = qreal(w) / qreal(h ? h : 1);
 
+    // Set near plane to 3.0, far plane to 7.0, field of view 45 degrees
+    const qreal zNear = 3.0, zFar = 100.0, fov = 45.0;
+
+    // Reset projection
+    projection.setToIdentity();
+
+    // Set perspective projection
+    projection.perspective(fov, aspect, zNear, zFar);
 }
 
 void OpenGLSimulation::paintGL()
--- openglsimulation.h
+++ openglsimulation.h
@@ -2,8 +2,15 @@
 #define OPENGLSIMULATION_H
 
 #include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QMatrix4x4>
+#include <QQuaternion>
+#include <QVector2D>
+#include <QBasicTimer>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLTexture>
 
-class OpenGLSimulation : public QOpenGLWidget
+class OpenGLSimulation : public QOpenGLWidget, protected QOpenGLFunctions
 {
     Q_OBJECT
 public:
@@ -13,6 +20,18 @@ public:
     void initializeGL() Q_DECL_OVERRIDE;
     void resizeGL(int w, int h) Q_DECL_OVERRIDE;
     void paintGL() Q_DECL_OVERRIDE;
+
+private:
+    void initShaders();
+    void initTextures();
+
+private:
+    QBasicTimer timer;
+
+    QOpenGLShaderProgram program;
+    QOpenGLTexture *texture;
+
+    QMatrix4x4 projectionMat;
 
 signals:
 
